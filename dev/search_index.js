a993f8d2734edbee6abca0f7374f303244ffd3e9
Documenter.jl <documenter@juliadocs.github.io>
2024-02-03 17:24:32 +0000

    build based on 83a6dd0

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Ding","category":"page"},{"location":"#Ding","page":"Home","title":"Ding","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Ding.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Ding]","category":"page"}]
+}
