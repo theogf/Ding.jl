9f34fc4eb39b7cd03cb6eb95f34e2dafaa3e09b3
theogf <theo.galyfajou@gmail.com>
2024-02-07 15:01:48 +0100

    use addenv

--- src/play.jl
+++ src/play.jl
@@ -1,10 +1,9 @@
 function play(file::AbstractString)
     ffplay() do exe
-        withenv(
+        run(
+            addenv(`$(exe) -nodisp -v 0 -autoexit $(file)`; wait = false),
             "ALSA_CONFIG_PATH" => "/usr/share/alsa/alsa.conf",
             "ALSA_PLUGIN_DIR" => "/usr/lib/alsa-lib/",
-        ) do
-            run(`$(exe) -nodisp -v 0 -autoexit $(file)`; wait = false)
-        end
+        )
     end
 end
