7465beb19e9182e793b69e00995a766b86c37cb6
theogf <theo.galyfajou@gmail.com>
2024-02-07 16:45:42 +0100

    fix use of addenv

--- src/play.jl
+++ src/play.jl
@@ -1,9 +1,12 @@
 function play(file::AbstractString)
     ffplay() do exe
         run(
-            addenv(`$(exe) -nodisp -v 0 -autoexit $(file)`; wait = false),
-            "ALSA_CONFIG_PATH" => "/usr/share/alsa/alsa.conf",
-            "ALSA_PLUGIN_DIR" => "/usr/lib/alsa-lib/",
+            addenv(
+                `$(exe) -nodisp -v 0 -autoexit $(file)`,
+                "ALSA_CONFIG_PATH" => "/usr/share/alsa/alsa.conf",
+                "ALSA_PLUGIN_DIR" => "/usr/lib/alsa-lib/",
+            );
+            wait = false,
         )
     end
 end
